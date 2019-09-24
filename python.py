4166fbbc40f8bac4a093de2ddae82dd4f0ec6df9
Leonard Melnik <leonard@onedr.io>
2019-09-24 10:41:36 -0400

    Cleaning Up

--- python.py
+++ python.py
@@ -16,14 +16,15 @@ import requests
 
 driver = webdriver.Chrome("E:\Github\chromedriver")
 driver.maximize_window()
+
 full = []
-course=[]
-assignment=[]
-due=[]
+
 driver.get("https://login-learn.k12.com/#schedule")
+
 driver.implicitly_wait(5)
+
 username = driver.find_element_by_name('username')
-#username = driver.find_element_by_id("input-text-username")
+
 password = driver.find_element_by_name('password')
 login = driver.find_element_by_id('c97')
 
@@ -33,8 +34,7 @@ login.click()
 driver.implicitly_wait(5)
 driver.find_element_by_link_text('MY SCHEDULE').click()
 driver.implicitly_wait(5)
-#nothing = driver.find_elements_by_class_name('empty-message')
-#publicSpeaking = driver.find_element_by_link_text('ENG020 Summit Public Speaking')
+
 def check_exists_by_xpath(xpath):
     try:
         driver.find_element_by_xpath(xpath)
@@ -49,6 +49,8 @@ def recursive():
         i = 1
         if(check_exists_by_xpath('//*[@id="components"]/div/div[2]/div/div[1]/div/div[3]/table/tbody/tr/td[1]/div/div[2]/div/div/a') == True):
             while (g == True):  # What happens per day
+
+                #Defining Paths BEFORE GRABBING DATA
                 tbodyXpath = '//*[@id="components"]/div/div[2]/div/div[1]/div/div[3]/table/tbody/tr[' + str(
                     i) + ']/td[1]/div/div[2]/div/div/a'
                 assignmentName = '//*[@id="components"]/div/div[2]/div/div[1]/div/div[3]/table/tbody/tr[' + str(
@@ -57,22 +59,32 @@ def recursive():
                     i) + ']/td[3]/div/div/span'
                 DueDate = '/html/body/div/section/div[3]/div[2]/div[4]/div[2]/div[2]/div/div[1]/div/div[1]/form/div[2]/div/p'
                 if (check_exists_by_xpath(tbodyXpath) == True):
+
                     courseCurrent = driver.find_element_by_xpath(tbodyXpath).get_property('title')
                     assignmentCurrent = driver.find_element_by_xpath(assignmentName).get_property('title').replace(',','')
                     assignmentDueTime = driver.find_element_by_xpath(DueTime).text
+                    #Clean up time due since sometimes text is included
                     if "Due " in assignmentDueTime:
                         assignmentDueTime = assignmentDueTime.replace('Due ','')
+
                     assignmentDueDate = driver.find_element_by_xpath(DueDate).text
                     assignmentDueDate = str(datetime.strptime(assignmentDueDate, '%B %d, %Y'))[0:10]
+
+                    #Trello API
                     querystring = {"name": assignmentName, "desc": courseCurrent, "pos": "bottom","due":assignmentDueDate,
                                    "idList": "5d828f95517f922a4c16833d", "keepFromSource": "all", "key": keys.apiKey,
                                    "token": keys.tokenKey}
-
+                    #Push info to api
                     response = requests.request("POST", url, params=querystring)
+
                     print(response.text)
+
                     full.append([courseCurrent, assignmentCurrent,assignmentDueDate,assignmentDueTime])
+
+
                     dateTime = driver.find_element_by_xpath(pressNext).get_property('href')
                     due.append(dateTime)
+
                     i = i + 1
                 elif (check_exists_by_xpath(tbodyXpath) == False):  # if no more assignments
                     g = False
@@ -80,4 +92,5 @@ def recursive():
         time.sleep(1)
 recursive()
 
+#Save to csv for chrome export
 np.savetxt('out.csv', (np.array(full)), delimiter=",", fmt = '%s',header='Description,Subject,Start Date, End Time')
